ea6e7b5210dd1ea17d78a72104a84642add5ed84
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:17:44 +0530

    [arvind] Add: branch2 created

---
+++ FunctionalCode/PowerTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class PowerTwo
+    {
+        int basenumber, exponent, power, i; 
+        public void powerpro()
+        {
+            Console.Write("Enter any number");
+            basenumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter any power: ");
+            exponent = Convert.ToInt32(Console.ReadLine());
+            power = 1;
+            i = 1;
+            while (i <= exponent) 
+            {
+                power = power * basenumber;
+                i++;
+            }
+            Console.Write("Power : " + power);
+            Console.ReadLine();
+        }
+    }
+}
