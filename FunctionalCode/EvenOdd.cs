d78224ec44fe11691d79acfe63de6f03318db5c7
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:24:12 +0530

    [arvind] Add: branch4 created

---
+++ FunctionalCode/EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class EvenOdd
+    {
+        public void checkev()
+        {
+            Console.WriteLine("enter any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("this is even number");
+            }
+            else
+            {
+                Console.WriteLine("this is odd number");
+            }
+        }
+    }
+}
