71b4e55ea24e212981d9f6ee64cb6eacd9979084
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:21:17 +0530

    [arvind] Add: branch3 created

---
+++ FunctionalCode/AlphVow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class AlphVow
+    {
+        char ch;
+        public void alfavow()
+        {
+            Console.Write("use input as  Alphabet (A-Z or a-z):");
+            ch = Convert.ToChar(Console.ReadLine().ToLower());
+            int i = ch;
+            if (i >= 48 && i <= 57)
+            {
+                Console.Write("Please enter an alpahbet number.");
+            }
+            else
+            {
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is not a vowel");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
