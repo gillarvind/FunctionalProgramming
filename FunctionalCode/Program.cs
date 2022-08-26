f3aab54923f86f3805c0cb10bf5c903efc5d8a34
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:07:21 +0530

    [arvind] Add: folder structure created

---
+++ FunctionalCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
