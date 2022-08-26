038b81cb545b4d4167c2b3d09a77f804c366fbf5
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:30:21 +0530

    [arvind] Add: branch6 created

---
+++ FunctionalCode/HarmonicNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class HarmonicNumber
+    {
+        int num = 5;
+        double result = 0.0;
+        public void nharmonic()
+        {
+            while (num > 0)
+            {
+                result += (double)1 / num;
+                num--;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
