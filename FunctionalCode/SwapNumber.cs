3f80392cc715199d9d758680b9c99e7e38c50a9b
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:33:16 +0530

    [arvind] Add: branch7 created

---
+++ FunctionalCode/SwapNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class SwapNumber
+    {
+        int num1 = 23;
+        int num2 = 45;
+        int temp;
+        public void swap()
+        {
+            Console.WriteLine("before swapping number");
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("after swapping number");
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+    }
+}
