8e05b5098ac53b90ce4d854c6213e32a87221992
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:36:34 +0530

    [arvind] Add: branch8 created

---
+++ FunctionalCode/Factors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class Factors
+    {
+        int num;
+        public void fact()
+        {
+            Console.WriteLine("enter any number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= num; i++)
+            {
+                while (num % i == 0)
+                {
+                    Console.WriteLine(i + " ");
+                    num = num / i;
+                }
+            }
+            if (num >= 2)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
