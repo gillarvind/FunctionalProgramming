1f220f5d63c7e38558d56c65d8dd8aa36c10b22e
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-26 19:39:27 +0530

    [arvind] Add: branch9 created

---
+++ FunctionalCode/QuoRem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class QuoRem
+    {
+        int divident = 50, divisor = 8;
+        public void quorem()
+        {
+            Console.WriteLine("enter any divident number:");
+            int divident = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter any diviser:");
+            int divisor = int.Parse(Console.ReadLine());
+            int quotient = divident / divisor;
+            int reminder = divident % divisor;
+            Console.WriteLine("quotient-" + quotient);
+            Console.WriteLine("reminder-" + reminder);
+        }
+    }
+}
